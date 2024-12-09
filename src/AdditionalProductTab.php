2b22d353233cdb6bf9776b63fece394af19a288d
Vulvani <147606953+Vulvani@users.noreply.github.com>
2024-12-09 17:52:12 +0100

    Initial commit: Basic plugin structure with custom tab functionality

---
+++ src/AdditionalProductTab.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AdditionalProductTab;
+
+use Shopware\Core\Framework\Plugin;
+
+class AdditionalProductTab extends Plugin
+{
+    // Keine zusätzlichen Methoden erforderlich für dieses einfache Plugin
+}
